288e32660fd06971d8eb8ae51c30d3160ecb7d3b
Anatoli <78858463+kru4ara@users.noreply.github.com>
2026-08-22 23:30:33 +0300

    Update telegram-actions.ts

--- app/reminders/telegram-actions.ts
+++ app/reminders/telegram-actions.ts
@@ -1,6 +1,6 @@
 'use server';
 
-import { supabase } from '../lib/supabaseClient';
+import { supabase } from '../../lib/supabaseClient';
 import { revalidatePath } from 'next/cache';
 
 // Создаём Telegram-бот
