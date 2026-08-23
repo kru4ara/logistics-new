2f091147b9877d6deabe5dba6e6408e94b23c4b2
Anatoli <78858463+kru4ara@users.noreply.github.com>
2026-08-23 18:20:43 +0300

    Create trip-status-actions.ts

---
+++ app/driver/trip-status-actions.ts
@@ -0,0 +1,19 @@
+'use server';
+
+import { supabase } from '../../lib/supabaseClient';
+import { revalidatePath } from 'next/cache';
+
+export async function changeTripStatus(tripId: string, status: string) {
+  const { error } = await supabase
+    .from('trips')
+    .update({
+      status: status
+    })
+    .eq('id', tripId);
+
+  if (error) {
+    throw new Error(`Ошибка загрузки: ${error.message}`);
+  }
+
+  revalidatePath(`/driver/trips/${tripId}`);
+}
