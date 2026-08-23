47dce69b5c079f4ceb9441355c815fe556e4d82d
Anatoli <78858463+kru4ara@users.noreply.github.com>
2026-08-23 12:41:44 +0300

    Update page.tsx

--- app/reports/page.tsx
+++ app/reports/page.tsx
@@ -1,4 +1,4 @@
-import { supabase } from '../lib/supabaseClient';
+import { supabase } from '../../lib/supabaseClient';
 import { Button } from '@/components/ui/button';
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card';
 
