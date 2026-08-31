19c5e57f62dc2262d981309d49aacae6649f0858
Anatoli <78858463+kru4ara@users.noreply.github.com>
2026-08-31 09:28:53 +0300

    Update page.tsx

--- app/clients/page.tsx
+++ app/clients/page.tsx
@@ -6,6 +6,7 @@ export default async function ClientsPage() {
   const { data: clients, error } = await supabase
     .from('clients')
     .select('*');
+
   if (error) {
     return <div>Ошибка загрузки: {error.message}</div>;
   }
