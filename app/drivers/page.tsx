43cc49c57d1a3fc02bfcacdc33a525ed22e82c8e
Anatoli <78858463+kru4ara@users.noreply.github.com>
2026-08-31 09:29:28 +0300

    Update page.tsx

--- app/drivers/page.tsx
+++ app/drivers/page.tsx
@@ -6,6 +6,7 @@ export default async function DriversPage() {
   const { data: drivers, error } = await supabase
     .from('drivers')
     .select('*');
+
   if (error) {
     return <div>Ошибка загрузки: {error.message}</div>;
   }
