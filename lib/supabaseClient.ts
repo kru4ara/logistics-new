5d24b84be1ea49d75a4bc5dff308e5ccce5a180a
Anatoli <78858463+kru4ara@users.noreply.github.com>
2026-09-03 23:48:03 +0300

    Update supabaseClient.ts

--- lib/supabaseClient.ts
+++ lib/supabaseClient.ts
@@ -1,7 +1,13 @@
-import { createClient, type SupabaseClient } from '@supabase/supabase-js';
+import { createBrowserClient } from '@supabase/ssr';
+import { createClient as createSupabaseClient } from '@supabase/supabase-js';
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-export const supabase = createClient(supabaseUrl, supabaseAnonKey);
-export { createClient };
+// Старый клиент для всех файлов
+export const supabase = createSupabaseClient(supabaseUrl, supabaseAnonKey);
+
+// Функция для логина
+export function createClient() {
+  return createBrowserClient(supabaseUrl, supabaseAnonKey);
+}
